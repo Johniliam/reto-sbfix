b1fa15804883f0caf73138587944fc31666b506e
Johnatan Ramirez <johniliam2151@gmail.com>
2021-12-02 15:01:28 -0600

    :tada: Initial commit

---
+++ landing-page/src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+
+import { AuthService } from "src/app/services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  login(){
+    this.authService
+      .login( this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe();
+    // console.log('login')
+  }
+
+}
